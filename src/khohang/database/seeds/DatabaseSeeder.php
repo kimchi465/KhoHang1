a1607a367c7498d490e0e7b01b2f2b8473d4c2a6
nguyenkimchi <nguyenkimchi468@gmail.com>
2020-01-11 18:11:56 +0700

    Bổ sung DatabaseSeeder

--- src/khohang/database/seeds/DatabaseSeeder.php
+++ src/khohang/database/seeds/DatabaseSeeder.php
@@ -14,5 +14,19 @@ class DatabaseSeeder extends Seeder
         // sử dụng cho câu lệnh "php artisan db:seed"
         $this->call(KhohangTableSeeder::class);
         $this->call(LoaiTableSeeder::class);
+        $this->call(KhachhangSeeder::class);
+        $this->call(QuyenTableSeeder::class);
+        $this->call(NhanvienTableSeeder::class);
+        $this->call(KhuyenmaiTableSeeder::class);
+        $this->call(XuatxuTableSeeder::class);
+        $this->call(SanphamTableSeeder::class);
+        $this->call(ThanhtoanTableSeeder::class);
+        $this->call(VanchuyenTableSeeder::class);
+        $this->call(NhacungcapTableSeeder::class);
+        $this->call(NhapkhoTableSeeder::class);
+        $this->call(ChitietnhapkhoTableSeeder::class);
+        $this->call(XuatkhoTableSeeder::class);
+        $this->call(ChitietxuatkhoTableSeeder::class);
+        
     }
 }
